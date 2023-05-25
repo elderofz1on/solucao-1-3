0340260f3b93feaf69f866b24802cc71df5f9e7e
Wagner <wprodriguesbr@gmail.com>
2023-05-25 10:16:37 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.LeftTriangle)
+basic.pause(2000)
+basic.showIcon(IconNames.Square)
+basic.pause(2000)
+basic.showIcon(IconNames.Target)
+basic.pause(2000)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
